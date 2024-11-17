09f94b1c6101301838cdc83a0d06471d541fa98d
zohaib <muhammadzohaibmalik10@gmail.com>
2024-11-18 02:07:25 +0500

    connected to database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import { config } from './config/config.js';
+
 const app = express();
 
+// Connect to MongoDB
+connectDB();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
-const port = process.env.PORT || 3000;
+const port =config.port || 3000;
+
 // Basic route
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the server!' });

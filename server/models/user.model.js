4c562648201a7bbf4ca2e4a24b7530560b0d0ff0
zohaib <muhammadzohaibmalik10@gmail.com>
2024-11-18 06:44:34 +0500

    added user model

---
+++ server/models/user.model.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      minlength: 3,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+      select: false,
+      trim: true,
+    },
+    role: {
+      type: String,
+      enum: ["instructor", "student"],
+      default: "student",
+    },
+    enrolledCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", userSchema);

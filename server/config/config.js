44f647d9d8fd48d0676b1be4609ae81b7f93c4d0
zohaib <muhammadzohaibmalik10@gmail.com>
2024-11-19 01:54:41 +0500

    add jwt secret key

--- server/config/config.js
+++ server/config/config.js
@@ -5,6 +5,6 @@ conf();
 const _config = {
   port: process.env.PORT,
   databaseUrl: process.env.MONGO_URI,
+  jwt_secret: process.env.JWT_SECRET,
 };
-
 export const config = Object.freeze(_config);

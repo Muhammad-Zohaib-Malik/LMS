b07c41ca8dd9bd48b6e40cad74fa2e1131e1541d
zohaib <muhammadzohaibmalik10@gmail.com>
2024-11-18 06:33:56 +0500

    added utils classes

---
+++ server/utils/asyncHandler.js
@@ -0,0 +1,9 @@
+  export const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+      await fn(req, res, next)
+    } catch (err) {
+      res.status(err.code || 500).json({ sucess: false, message: err.message })
+
+    }
+
+  }

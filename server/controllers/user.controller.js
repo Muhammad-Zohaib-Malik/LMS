59a3b5b52281cac05cefaf23d9eb1c9f69e2625c
zohaib <muhammadzohaibmalik10@gmail.com>
2024-11-19 01:20:22 +0500

    fix bugs in register controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,4 +1,3 @@
-import User from "../models/user.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
@@ -23,12 +22,14 @@ export const registerUser = asyncHandler(async (req, res) => {
 
   // Create new user
   const user = await User.create({
-    username,
+    name,
     email,
     password: hashedPassword,
   });
 
+  const newUser = await User.findById(user._id).select("-password");
+
   res
     .status(200)
-    .json(new ApiResponse(200, user, "User Registered Successfully"));
+    .json(new ApiResponse(200, newUser, "User Registered Successfully"));
 });

a951a968d0fdc734282ff85cb3e91d4361b556df
zohaib <muhammadzohaibmalik10@gmail.com>
2024-11-19 17:33:25 +0500

    added update User Profile

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -4,14 +4,22 @@ import {
   loginUser,
   logoutUser,
   registerUser,
+  updateUserProfile,
 } from "../controllers/user.controller.js";
 import { verify } from "../middlewares/verify.js";
+import { upload } from "../middlewares/multer.js";
 
 const userRouter = express.Router();
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.post("/logout", logoutUser);
-userRouter.get("/getProfile", verify, getUserProfile);
+userRouter.get("/get-profile", verify, getUserProfile);
+userRouter.put(
+  "/update-profile",
+  upload.single("profilePic"),
+  verify,
+  updateUserProfile
+);
 
 logoutUser;
 

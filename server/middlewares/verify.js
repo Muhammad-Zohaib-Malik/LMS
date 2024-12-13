97f02a9402d487037967e96dce7440c63558f36e
zohaib <muhammadzohaibmalik10@gmail.com>
2024-12-13 07:22:54 +0500

    remove bugs

--- server/middlewares/verify.js
+++ server/middlewares/verify.js
@@ -2,21 +2,16 @@ import jwt from "jsonwebtoken";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { config } from "../config/config.js";
+import { User } from "../models/user.model.js";
 
 export const verify = asyncHandler(async (req, _, next) => {
   const token = req.cookies?.jwt;
   if (!token) {
     throw new ApiError(400, "Unauthorized User");
   }
-  console.log(token);
 
-  const decode = await jwt.verify(token, config.jwt_secret);
-  if (!decode) {
-    throw new ApiError(400, "Invalid Token");
-  }
-
-  console.log(decode);
-  req.id = decode.userId;
-
+  const decoded = await jwt.verify(token, config.jwt_secret);
+  const user = await User.findById(decoded.userId).select("-password");
+  req.user = user;
   next();
 });
